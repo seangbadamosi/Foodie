5ceb6b5525f50803dfd8cba29bb94e92908b252b
Sean Gbadamosi <139792459+seangbadamosi@users.noreply.github.com>
2026-05-12 14:00:47 +0100

    added sidenav

--- index.js
+++ index.js
@@ -19,3 +19,26 @@ document.querySelectorAll(".faq-header").forEach((button) => {
 		}
 	});
 });
+
+// for side nav
+const sidenav = document.querySelector(".sidenav");
+const trigger = document.querySelector(".sidenav-trigger");
+
+// CREATE OVERLAY
+const overlay = document.createElement("div");
+overlay.classList.add("overlay");
+document.body.appendChild(overlay);
+
+// OPEN SIDENAV
+trigger.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	sidenav.classList.add("active");
+	overlay.classList.add("active");
+});
+
+// CLOSE SIDENAV
+overlay.addEventListener("click", () => {
+	sidenav.classList.remove("active");
+	overlay.classList.remove("active");
+});
